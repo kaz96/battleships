a6ccb27601a6aad993f9efc81fa252fb7c19c0fb
Lyhuy55 <lyhuy55@gmail.com>
2017-05-01 13:25:07 +1000

    adding unmute button

--- src/GameController.cs
+++ src/GameController.cs
@@ -152,58 +152,63 @@ public static class GameController
 	/// <remarks>
 	/// Displays a message, plays sound and redraws the screen
 	/// </remarks>
-	private static void AttackCompleted(object sender, AttackResult result)
-	{
-		bool isHuman = false;
-		isHuman = object.ReferenceEquals(_theGame.Player, HumanPlayer);
+private static void AttackCompleted (object sender, AttackResult result)
+{
+	bool isHuman = false;
+	isHuman = object.ReferenceEquals (_theGame.Player, HumanPlayer);
 
-		if (isHuman) {
-			UtilityFunctions.Message = "You " + result.ToString();
-		} else {
-			UtilityFunctions.Message = "The AI " + result.ToString();
-		}
-
-		if (SwinGame.KeyTyped (KeyCode.vk_m))
-		{
-
-
-
-		}
-
-
-		switch (result.Value) {
-			case ResultOfAttack.Destroyed:
-				PlayHitSequence(result.Row, result.Column, isHuman);
-				Audio.PlaySoundEffect(GameResources.GameSound("Sink"));
-
-				break;
-			case ResultOfAttack.GameOver:
-				PlayHitSequence(result.Row, result.Column, isHuman);
-				Audio.PlaySoundEffect(GameResources.GameSound("Sink"));
-
-				while (Audio.SoundEffectPlaying(GameResources.GameSound("Sink"))) {
-					SwinGame.Delay(10);
-					SwinGame.RefreshScreen();
-				}
-
-				if (HumanPlayer.IsDestroyed) {
-					Audio.PlaySoundEffect(GameResources.GameSound("Lose"));
-				} else {
-					Audio.PlaySoundEffect(GameResources.GameSound("Winner"));
-				}
-
-				break;
-			case ResultOfAttack.Hit:
-				PlayHitSequence(result.Row, result.Column, isHuman);
-				break;
-			case ResultOfAttack.Miss:
-				PlayMissSequence(result.Row, result.Column, isHuman);
-				break;
-			case ResultOfAttack.ShotAlready:
-				Audio.PlaySoundEffect(GameResources.GameSound("Error"));
-				break;
-		}
+	if (isHuman) {
+		UtilityFunctions.Message = "You " + result.ToString ();
+	} else {
+		UtilityFunctions.Message = "The AI " + result.ToString ();
 	}
+
+	switch (result.Value) {
+
+
+	case ResultOfAttack.Destroyed:
+		if (GameResources.statusEffect == true) {
+			PlayHitSequence (result.Row, result.Column, isHuman);
+			Audio.PlaySoundEffect (GameResources.GameSound ("Sink"));
+		}
+
+		break;
+	case ResultOfAttack.GameOver:
+		if (GameResources.statusEffect == true) {
+			PlayHitSequence (result.Row, result.Column, isHuman);
+			Audio.PlaySoundEffect (GameResources.GameSound ("Sink"));
+		}
+
+		if (GameResources.statusEffect == true) {
+			while (Audio.SoundEffectPlaying (GameResources.GameSound ("Sink"))) {
+				SwinGame.Delay (10);
+				SwinGame.RefreshScreen ();
+			}
+		}
+
+		if (HumanPlayer.IsDestroyed) {
+			if (GameResources.statusEffect == true) {
+				Audio.PlaySoundEffect (GameResources.GameSound ("Lose"));
+			}
+		} else {
+			Audio.PlaySoundEffect (GameResources.GameSound ("Winner"));
+		}
+
+		break;
+	case ResultOfAttack.Hit:
+		PlayHitSequence (result.Row, result.Column, isHuman);
+		break;
+	case ResultOfAttack.Miss:
+		PlayMissSequence (result.Row, result.Column, isHuman);
+		break;
+	case ResultOfAttack.ShotAlready:
+		if (GameResources.statusEffect == true) {
+			Audio.PlaySoundEffect (GameResources.GameSound ("Error"));
+		}
+		break;
+
+	}
+}
 
 	/// <summary>
 	/// Completes the deployment phase of the game and
@@ -288,6 +293,11 @@ public static class GameController
 		{
 			GameResources.Mute ();
 		}
+
+		if (SwinGame.KeyTyped(KeyCode.vk_p)) //Lyhuy
+        {
+            GameResources.Play();
+        }
 
 
 		switch (CurrentState) {
