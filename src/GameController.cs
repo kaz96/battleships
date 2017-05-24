b78fa4a0ad08c155836f3da627ff3e09843ee405
shub3 <shubchuadhary03@gmail.com>
2017-05-24 10:59:10 +1000

    Fixed highscores saving error

--- src/GameController.cs
+++ src/GameController.cs
@@ -190,7 +190,7 @@ public static class GameController
                 {
                     while (Audio.SoundEffectPlaying(GameResources.GameSound("Sink")))
                     {
-                        SwinGame.Delay(10);
+                        SwinGame.Delay(3);
                         SwinGame.RefreshScreen();
                     }
                 }
